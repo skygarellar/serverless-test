27f99f5352c6bf7017211a14a4b2cbd11b6e5e37
Roberto Garella <roberto.garella@skytv.it>
2020-03-06 11:47:08 +0100

    First

--- handler.js
+++ handler.js
@@ -1,23 +1,27 @@
 'use strict';
 
-const { TeamsStaticContentGenerator } = require('reactdomserver-test/build');
+const {
+  TeamsStaticContentGenerator
+} = require('reactdomserver-test/lib');
 
 module.exports.test = async event => {
+  let data;
+  try {
+    data = await TeamsStaticContentGenerator.get({
+      competition: 21
+    });
+    console.log(data);
+  } catch (error) {
+    data = error;
+  }
 
-  const data = await TeamsStaticContentGenerator.get({competition: 21});
-  console.log(data);
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
+    body: JSON.stringify(data,
       null,
       2
     ),
   };
-
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
